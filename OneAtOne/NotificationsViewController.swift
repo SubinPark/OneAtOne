a2dd8c1e5ba5dfcf33df07266feaa7c6d941acbc
Kate Finlay <kate_finlay@intuit.com>
2017-01-04 19:57:14 -0800

    Make buttons to spec
    - also deletes collage image
    - also adds feedback email text (not clickable yet)

--- OneAtOne/NotificationsViewController.swift
+++ OneAtOne/NotificationsViewController.swift
@@ -39,5 +39,6 @@ class NotificationsViewController: UIViewController {
         
         // Round corners
         confirmButton.layer.cornerRadius = confirmButton.frame.height/2.0
+       confirmButton.leadTitle(withFontAwesomeIconNamed: "fa-thumbs-up")
     }
 }
---
+++ OneAtOne/UIButton+icon.swift
@@ -0,0 +1,25 @@
+//
+//  UIButton+icon.swift
+//  OneAtOne
+//
+//  Created by Finlay, Kate on 1/4/17.
+//  Copyright © 2017 1@1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+internal extension UIButton {
+    
+    func leadTitle(withFontAwesomeIconNamed: String) {
+        if let titleText = self.titleLabel?.text {
+            let buttonString = String.fontAwesomeString(name: "fa-share") + " " + titleText
+            let buttonStringAttributed = NSMutableAttributedString(string: buttonString, attributes: [NSFontAttributeName:UIFont.systemFont(ofSize: 13.0)])
+            buttonStringAttributed.addAttribute(NSFontAttributeName, value: UIFont.iconFontOfSize(font: "FontAwesome", fontSize: 14), range: NSRange(location: 0,length: 1))
+            
+            self.setAttributedTitle(buttonStringAttributed, for: .normal)
+        }
+
+    }
+
+}
