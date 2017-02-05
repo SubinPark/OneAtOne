0cfe3e38a118911b056bb66009ffa79d336da07f
Kate Finlay <kate_finlay@intuit.com>
2017-02-05 14:19:37 -0800

    Get description text for youtube videos, make description scrollable

--- OneAtOne/ViewController.swift
+++ OneAtOne/ViewController.swift
@@ -16,10 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var viewCountLoadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         shareButton.leadTitle(withFontAwesomeIconNamed: "fa-share")
+        // Start description text view at top of text ins
+        descriptionTextView.scrollRangeToVisible(NSMakeRange(0, 0))
     }
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -28,43 +31,47 @@ class ViewController: UIViewController {
 		viewCountLoadingIndicator.startAnimating()
 		
 		RCValues.sharedInstance.fetchCloudValues { (videoUrl) in
-			YoutubeUtils.getViewCount(for: videoUrl) { (error : Error?, viewCount : Int?) in
-				DispatchQueue.main.async {
-					self.viewCountLoadingIndicator.stopAnimating()
-					self.viewCountLoadingIndicator.isHidden = true
-					
-					if let error = error {
-						print("Error getting view count: \(error)")
-					}
-					
-					if let viewCount = viewCount {
-						
-						// Format the view count
-						let numberFormatter = NumberFormatter()
-						numberFormatter.numberStyle = NumberFormatter.Style.decimal
-						
-						if let formattedViewCount = numberFormatter.string(from: NSNumber(value: viewCount)) {
-							self.viewCountLabel.text = "\(formattedViewCount) views"
-							self.viewCountLabel.isHidden = false
-						}
-					}
-				}
-			}
-			
-			YoutubeUtils.getTitle(for: videoUrl) { (error : Error?, title : String?) in
-				DispatchQueue.main.async {
-					if let error = error {
-						print("Error getting title: \(error)")
-					}
-					if let title = title {
-						self.titleLabel.text = title
-					}
-				}
-			}
+            YoutubeUtils.getVideoInformation(for: videoUrl, callback: { (error, info : (viewCount: Int?, title: String?, description: String?)?) in
+                DispatchQueue.main.async {
+                    self.viewCountLoadingIndicator.stopAnimating()
+                    self.viewCountLoadingIndicator.isHidden = true
+                    
+                    if let error = error {
+                        print("Error getting view count: \(error)")
+                    }
+                    if let info = info {
+                        if let viewCount = info.viewCount {
+                            
+                            // Format the view count
+                            let numberFormatter = NumberFormatter()
+                            numberFormatter.numberStyle = NumberFormatter.Style.decimal
+                            
+                            if let formattedViewCount = numberFormatter.string(from: NSNumber(value: viewCount)) {
+                                self.viewCountLabel.text = "\(formattedViewCount) views"
+                                self.viewCountLabel.isHidden = false
+                            }
+                        }
+                        if let title = info.title {
+                            self.titleLabel.text = title
+                        }
+                        if let description = info.description {
+                            self.descriptionTextView.text = description
+                            self.descriptionTextView.flashScrollIndicators()
+                        }
+                    }
+
+                }
+            })
 			
 			self.playerView.load(withVideoId: videoUrl)
 		}
 	}
+    
+    /// Reset UITextView to the top of the text instead of the middle.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        descriptionTextView.setContentOffset(CGPoint.zero, animated: false)
+    }
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
